d74362d1c22920b49d3baca82124a8c8e817472a
ArgamTL <43552997+ArgamTL@users.noreply.github.com>
2025-07-10 13:59:07 +0300

    Create task4.py

---
+++ task4/task4.py
@@ -0,0 +1,19 @@
+#############
+# Задание 4 #
+#############
+import sys
+#nums = sys.argv[1]
+
+#with open('nums.txt', 'r') as file:
+    #nums = [line.strip() for line in file]
+ 
+nums = [1, 2, 3, 4]
+
+def minMoves(nums) -> int:
+   total_sum = sum(nums)
+   min_value = min(nums)
+   moves = total_sum - min_value * len(nums)
+   return moves
+  
+print(minMoves( nums))   
+
