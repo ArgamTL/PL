96126c42441ff289d8b598987e67b4188be22900
ArgamTL <43552997+ArgamTL@users.noreply.github.com>
2025-07-11 16:18:12 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,15 +1,17 @@
 #############
 # Задание 1 #
 ############# 
-n = input("вводите n ")
-m = input("вводите m ")
-
+n = input("ENTER  n ")
+m = input("ENTER  m ")
+print("\nANSWER:")
 #n = 5
 #m = 4
 
 i = 1
 while True:
-print(i, end='')
-i = 1 + (i + int(m) - 2) % int(n)
-if i == 1:
-break
+    print(i, end='')
+    i = 1 + (i + int(m) - 2) % int(n)
+    if i == 1:
+        break
+
+###########################################
